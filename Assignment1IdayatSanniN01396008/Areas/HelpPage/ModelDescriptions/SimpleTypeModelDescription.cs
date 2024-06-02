69a7ad098f4188aa1a0865dc2d31e65278fa8403
Idayat Sanni <idayatsanni@gmail.com>
2024-06-01 21:24:36 -0400

    Add project files.

---
+++ Assignment1IdayatSanniN01396008/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Assignment1IdayatSanniN01396008.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Assignment1IdayatSanniN01396008/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Assignment1IdayatSanniN01396008.WebApiApplication" Language="C#" %>
